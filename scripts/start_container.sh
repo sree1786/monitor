8ffc05669b4555bb9169ecaa53a9f76a325bbeb5
sree1786 <62541317+sree1786@users.noreply.github.com>
2023-07-22 19:42:26 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+
+docker pull sree1786/aws-monitor-project
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 sree1786/aws-monitor-project
